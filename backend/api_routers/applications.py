b6ae471c10481ea73f3a839d6a60218de1ef21fb
AdnanSaeed-85 <saeedadnan151@gmail.com>
2026-08-14 13:11:29 +0500

    working on application endpoint

--- backend/api_routers/applications.py
+++ backend/api_routers/applications.py
@@ -1,8 +1,20 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from models.application import Application
-from schemas.application import ApplicationOutput, ApplicationResponse
-from core.dependencies import get_db
+from backend.models.application import Application
+from backend.schemas.application import ApplicationOutput, ApplicationResponse
+from backend.core.dependencies import get_db
 
-router = APIRouter(prefix="/api/appliaction", tags=['Application_Section'])
+router = APIRouter(prefix="/api/application", tags=['Application_Section'])
+
+@router.get('/all', response_model=list[ApplicationResponse], status_code=200)
+def get_all(db: Session = Depends(get_db)):
+    app = db.query(Application).all()
+    return app
+
+@router.get('/{application_id}', response_model=ApplicationResponse, status_code=200)
+def get_one(application_id: str, db: Session = Depends(get_db)):
+    single_id = db.query(Application).filter(Application.application_id == application_id).first()
+    if not single_id:
+        raise HTTPException(status_code=404, detail="Application not found!")
+    return single_id
 
--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from backend.api_routers.auth import router as auth_router
 from backend.api_routers.job_descriptions import router as jd_des_router
 from backend.api_routers.candidates import router as candidate_router
+from backend.api_routers.hitl import router as hitl_router
 
 app = FastAPI(title="HireLoop", version="1.0.0")
 
 app.include_router(auth_router)
 app.include_router(jd_des_router)
 app.include_router(candidate_router)
+app.include_router(hitl_router)
 
 @app.get("/")
 def root():
